80709ad7085186e477a981b61dd004121403651d
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2024-06-28 17:39:24 +0900

    feat: server.js 추가

---
+++ server.js
@@ -0,0 +1,35 @@
+const setup = require("./db_setup");
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+const session = require("express-session");
+app.use(
+  session({
+    secret: "암호화키",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+const cookieParser = require("cookie-parser");
+app.use(cookieParser());
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use("/", require("./routes/account"));
+app.use("/", require("./routes/post"));
+
+app.get("/", async (req, res) => {
+  try {
+    res.render("index.ejs");
+  } catch (err) {
+    res.status(500).send("DB Fail.");
+  }
+});
+
+app.listen(process.env.WEB_PORT, async () => {
+  await setup();
+});
