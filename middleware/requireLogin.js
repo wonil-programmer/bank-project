2677e4eaba958b1bbe4e127489e93eef6d3a58e9
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2024-06-29 21:24:23 +0900

    move: 세션 확인 함수 위치 변경

--- middleware/requireLogin.js
+++ middleware/requireLogin.js
@@ -1,7 +1,7 @@
 // 세션 확인 미들웨어 정의
 const requireLogin = (req, res, next) => {
   if (!req.session.user) {
-    res.redirect("/");
+    res.render("index.ejs", { data: { deniedAccess: true } });
   } else {
     next();
   }
